82180bf44f9f06dd795d1304ba6e6c19c158337e
andrmash <39763510+andrmash@users.noreply.github.com>
2018-05-31 13:26:43 +0200

    Update Assessment.py

--- Assessment.py
+++ Assessment.py
@@ -24,9 +24,9 @@ def classAvailability(age):
 #test 1 - 7 month old allocated to class 2 which empty - should return TRUE
 dict = [
 {'Name': 'Class1', 'minAge': 0, 'maxAge': 6, 'capacity' : 5, "infantsCapacity" : 5,"infantsEnrolled" : 5, "currentlyEnrolled" : 5},  ## check infant age group
-{'Name': 'Class2', 'minAge': 0, 'maxAge': 24, 'capacity' : 10, "infantsCapacity" : 2,"infantsEnrolled" : 0, "currentlyEnrolled" : 0},
+{'Name': 'Class2', 'minAge': 0, 'maxAge': 23, 'capacity' : 10, "infantsCapacity" : 2,"infantsEnrolled" : 0, "currentlyEnrolled" : 0},
 {'Name': 'Class3', 'minAge': 24, 'maxAge': 36, 'capacity' : 15, "infantsCapacity" : 0,"infantsEnrolled" : 0, "currentlyEnrolled" : 0},
-{'Name': 'Class4', 'minAge': 48, 'maxAge': 60, 'capacity' : 15, "infantsCapacity" : 0,"infantsEnrolled" : 0, "currentlyEnrolled" : 0},
+{'Name': 'Class4', 'minAge': 37, 'maxAge': 60, 'capacity' : 15, "infantsCapacity" : 0,"infantsEnrolled" : 0, "currentlyEnrolled" : 0},
 ]
 
 print classAvailability(7)
@@ -34,9 +34,9 @@ print classAvailability(7)
 #test 2 - infant class is full should overflow to empty 'class2' ie return true
 dict = [
 {'Name': 'Class1', 'minAge': 0, 'maxAge': 6, 'capacity' : 5, "infantsCapacity" : 5,"infantsEnrolled" : 5, "currentlyEnrolled" : 5},  ## check infant age group
-{'Name': 'Class2', 'minAge': 0, 'maxAge': 24, 'capacity' : 10, "infantsCapacity" : 2,"infantsEnrolled" : 0, "currentlyEnrolled" : 0},
+{'Name': 'Class2', 'minAge': 0, 'maxAge': 23, 'capacity' : 10, "infantsCapacity" : 2,"infantsEnrolled" : 0, "currentlyEnrolled" : 0},
 {'Name': 'Class3', 'minAge': 24, 'maxAge': 36, 'capacity' : 15, "infantsCapacity" : 0,"infantsEnrolled" : 0, "currentlyEnrolled" : 0},
-{'Name': 'Class4', 'minAge': 48, 'maxAge': 60, 'capacity' : 15, "infantsCapacity" : 0,"infantsEnrolled" : 0, "currentlyEnrolled" : 0},
+{'Name': 'Class4', 'minAge': 37, 'maxAge': 60, 'capacity' : 15, "infantsCapacity" : 0,"infantsEnrolled" : 0, "currentlyEnrolled" : 0},
 ]
 
 print classAvailability(5)
@@ -44,9 +44,9 @@ print classAvailability(5)
 #test 3 - out or range age should retturn false
 dict = [
 {'Name': 'Class1', 'minAge': 0, 'maxAge': 6, 'capacity' : 5, "infantsCapacity" : 5,"infantsEnrolled" : 5, "currentlyEnrolled" : 5},  ## check infant age group
-{'Name': 'Class2', 'minAge': 0, 'maxAge': 24, 'capacity' : 10, "infantsCapacity" : 2,"infantsEnrolled" : 0, "currentlyEnrolled" : 0},
+{'Name': 'Class2', 'minAge': 0, 'maxAge': 23, 'capacity' : 10, "infantsCapacity" : 2,"infantsEnrolled" : 0, "currentlyEnrolled" : 0},
 {'Name': 'Class3', 'minAge': 24, 'maxAge': 36, 'capacity' : 15, "infantsCapacity" : 0,"infantsEnrolled" : 0, "currentlyEnrolled" : 0},
-{'Name': 'Class4', 'minAge': 48, 'maxAge': 60, 'capacity' : 15, "infantsCapacity" : 0,"infantsEnrolled" : 0, "currentlyEnrolled" : 0},
+{'Name': 'Class4', 'minAge': 37, 'maxAge': 60, 'capacity' : 15, "infantsCapacity" : 0,"infantsEnrolled" : 0, "currentlyEnrolled" : 0},
 ]
 
 print classAvailability(62)
@@ -62,6 +62,10 @@ dict = [
 ]
 
 print classAvailability(5)
+
+
+# typeError
+print classAvailability('a')
 
 
 
